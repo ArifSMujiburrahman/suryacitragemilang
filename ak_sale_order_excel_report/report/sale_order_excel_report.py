cb0785895eac80cd94d36d72b8db6f9a8e44a980
ArifSMujiburrahman <79817466+ArifSMujiburrahman@users.noreply.github.com>
2021-12-27 09:50:53 +0700

    Update sale_order_excel_report.py

--- ak_sale_order_excel_report/report/sale_order_excel_report.py
+++ ak_sale_order_excel_report/report/sale_order_excel_report.py
@@ -31,7 +31,7 @@ class PartnerXlsx(models.AbstractModel):
             customer_header_format = workbook.add_format({
                 'align': 'center', 'font_size': 13, 'border': 1})
             customer_format = workbook.add_format({
-                'align': 'center', 'font_size': 13, 'border': 1})
+                'align': 'center', 'font_size': 13, 'border': 1, 'bold': True})
             table_left = workbook.add_format(
                 {'align': 'left', 'bold': True, 'border': 1})
             table_right = workbook.add_format(
