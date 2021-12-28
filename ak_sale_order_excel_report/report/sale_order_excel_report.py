0e73bf733f963c1ab9bc5727ec7cf2179888f1bb
ArifSMujiburrahman <79817466+ArifSMujiburrahman@users.noreply.github.com>
2021-12-28 13:49:46 +0700

    Update sale_order_excel_report.py

--- ak_sale_order_excel_report/report/sale_order_excel_report.py
+++ ak_sale_order_excel_report/report/sale_order_excel_report.py
@@ -135,7 +135,7 @@ class PartnerXlsx(models.AbstractModel):
                 worksheet.write(row, 1, line.product_uom_qty, table_row_right)
                 worksheet.write(row, 2, line.price_unit, table_row_right)
                 #worksheet.write(row, 6, line.product_uom.id, table_row_right)
-                worksheet.write(row, 6, line.product_uom.uom_uom, table_row_right)
+                worksheet.write(row, 6, line.product_uom_category.id, table_row_right)
                 if display_discount and group:
                     worksheet.write(row, 3, line.discount, table_row_right)
                     if display_tax and line.tax_id:
